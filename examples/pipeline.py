b9024219b8a4f38a692545fcc25fe577863aecc5
Bhavik Kawli <kawli.bhavik88@gmail.com>
2019-04-28 10:54:08 -0700

    added mag and dir gradients

--- examples/pipeline.py
+++ examples/pipeline.py
@@ -121,6 +121,37 @@ def abs_sobel_thresh(img, orient='x', sobel_kernel=3, mag_thresh=(0, 255)):
     # Return the result
     return binary_output
 
+def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # Take both Sobel x and y gradients
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    # Calculate the gradient magnitude
+    gradmag = np.sqrt(sobelx**2 + sobely**2)
+    # Rescale to 8 bit
+    scale_factor = np.max(gradmag)/255 
+    gradmag = (gradmag/scale_factor).astype(np.uint8) 
+    # Create a binary image of ones where threshold is met, zeros otherwise
+    binary_output = np.zeros_like(gradmag)
+    binary_output[(gradmag >= mag_thresh[0]) & (gradmag <= mag_thresh[1])] = 255
+
+    return binary_output
+
+def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi/2)):
+    # Grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # Calculate the x and y gradients
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    # Take the absolute value of the gradient direction, 
+    # apply a threshold, and create a binary image result
+    absgraddir = np.arctan2(np.absolute(sobely), np.absolute(sobelx))
+    binary_output =  np.zeros_like(absgraddir)
+    binary_output[(absgraddir >= thresh[0]) & (absgraddir <= thresh[1])] = 255
+
+    # Return the binary image
+    return binary_output
 
 images = glob.glob(test_images_input_folder + '*.jpg')
 for fname in images:
@@ -129,9 +160,13 @@ for fname in images:
     outputfile=fname.replace(test_images_input_folder, test_images_output_folder)
     outputfile_SX=outputfile.replace('.jpg','_SX.jpg')
     outputfile_SY=outputfile.replace('.jpg','_SY.jpg')
+    outputfile_MAG=outputfile.replace('.jpg','_MAG.jpg')
+    outputfile_DIR=outputfile.replace('.jpg','_DIR.jpg')
     
     cv2.imwrite(outputfile_SX, abs_sobel_thresh(img,'x',3,(20,200)))
     cv2.imwrite(outputfile_SY, abs_sobel_thresh(img,'y',3,(20,200)))
+    cv2.imwrite(outputfile_MAG, mag_thresh(img,3,(20,200)))
+    cv2.imwrite(outputfile_DIR, dir_threshold(img,3,(0.7,1.3)))
 
     #mpimg.imsave(outputfile_SX, abs_sobel_thresh(img,'x',20,200))
     #mpimg.imsave(outputfile_SY, abs_sobel_thresh(img,'y',20,200))
