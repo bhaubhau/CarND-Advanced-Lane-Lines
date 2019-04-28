5061b5c3bb8958e229ca7ca201ad0523c4abe112
bhaubhau <bhaubhau@gmail.com>
2019-04-28 14:49:59 +0530

    applied distortion correction

--- examples/calibrate.py
+++ examples/calibrate.py
@@ -6,8 +6,13 @@ import os
 
 calibration_input_folder='../camera_cal/'
 calibration_output_folder='../camera_cal_output/'
+undistort_output_folder='../undistort_output/'
+
 if not os.path.exists(calibration_output_folder):
     os.mkdir(calibration_output_folder)
+
+if not os.path.exists(undistort_output_folder):
+    os.mkdir(undistort_output_folder)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 objp = np.zeros((6*9,3), np.float32)
@@ -34,10 +39,17 @@ for fname in images:
         imgpoints.append(corners)
 
         # Draw and display the corners
-        img = cv2.drawChessboardCorners(img, (9,6), corners, ret)
+        img_corners=np.copy(img)
+        img_corners = cv2.drawChessboardCorners(img_corners, (9,6), corners, ret)
         outputfile=fname.replace(calibration_input_folder, calibration_output_folder)
-        cv2.imwrite(outputfile, img)
+        cv2.imwrite(outputfile, img_corners)
         #cv2.imshow('img',img)
         #cv2.waitKey(500)
+
+        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img.shape[1:], None, None)
+        undist = cv2.undistort(img, mtx, dist, None, mtx)
+        outputfile=fname.replace(calibration_input_folder, undistort_output_folder)
+        cv2.imwrite(outputfile, img)
+
 
 #cv2.destroyAllWindows()
